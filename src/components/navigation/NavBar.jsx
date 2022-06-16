1d4946a9aa135782857fa685007295039f187a67
Sam <smolina@thoughtworks.com>
2022-06-16 08:23:34 -0500

    Small refactor

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
-import styled from 'styled-components'
 
 const NavBar = () => {
     return (
